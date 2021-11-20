a3757b58e01d817ac92d9bfd21b38d64ef9b3791
joao-pedro213 <joao.capobiango213@gmail.com>
2021-11-19 22:29:23 -0300

    Adding dotenv variables

--- src/services/rds_service.py
+++ src/services/rds_service.py
@@ -1,5 +1,8 @@
 import pymysql
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 class RDSService:
     def __init__(self):
@@ -10,10 +13,10 @@ class RDSService:
         try:
             print('Connecting to MYSQL server.')
             connection = pymysql.connect(
-                host='mrmgmt.cacesswihlex.us-east-1.rds.amazonaws.com',
-                user='admin',
-                passwd='admin1980',
-                db='stage',
+                host=os.environ.get('HOST'),
+                user=os.environ.get('USER'),
+                passwd=os.environ.get('PASSWORD'),
+                db=os.environ.get('DATABASE_NAME'),
                 connect_timeout=5,
                 cursorclass=pymysql.cursors.DictCursor
             )
